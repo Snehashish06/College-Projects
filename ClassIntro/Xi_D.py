5bc82b6c0f620531ec96c7df11c15c4bd32af31e
Snehashish Biswas <teamscenariox@gmail.com>
2022-11-24 17:07:24 +0530

    Done 💫

--- ClassIntro/Xi_D.py
+++ ClassIntro/Xi_D.py
@@ -1,3 +1,4 @@
+"""      Coded by Snehashish and Pradumya!!  """
 def class_Xi_D(bool, *args, **kwargs) -> str:
     if bool != True:
         return
@@ -9,7 +10,8 @@ def class_Xi_D(bool, *args, **kwargs) -> str:
     for x in range(43):
         i.append(x)
     all_info = [class_, f"{i[-1]}", f"{stream}", class_teacher]
-    str = f"We are the students of class: {all_info[0]}, our class teacher is: {all_info[3]}, strength of our class is: {all_info[1]}, we are {all_info[2]} students, our class captain is: "
+    all_info.extend(class_captain)
+    str = f"We are the students of class: {all_info[0]}, our class teacher is: {all_info[3]}, strength of our class is: {all_info[1]}, we are {all_info[2]} students, our class captains are: {all_info[4]}, {all_info[5]}..."
     return str
 
-print(class_Xi_D(True))
+print(f"{class_Xi_D(True)}\n\n{__doc__}")
