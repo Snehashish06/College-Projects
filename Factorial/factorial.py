765740c262f5f0d17bc54fd713754b8c46db22e2
Snehashish Biswas <teamscenariox@gmail.com>
2023-07-04 15:46:30 +0530

    Create factorial.py

---
+++ Factorial/factorial.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class Factorial:
+
+
+    def __init__(self):
+        print(f"\033[1m\033[101m\033[35mCoded by Snehashish (CoderX)\033[0m:") #!   Credits
+
+    def with_logical_work(self, number: Optional[int] = 0) : # Takes only one parameter and is optional
+        if number < 0:raise ValueError(f'Can\'t use negative {number = }') # if negative raise ValueError
+        factorial_ = 1
+        for i in range(1, number+1):factorial_ *= i # increment factorial_ variable to get the end result
+        return factorial_ # return Value
+    
+    def without_logical_work(self, num: Optional[int] = 0):
+        ''' well you didn't specify to write whole logic or to write from scratch tho I am writing both with and without logic'''
+        return __import__('math').factorial(num)
+
+    def call_both(self, num: int):
+        return f"{self.with_logical_work(num)}\n{self.without_logical_work(num)}"
+
+    def siso(*args, **kwargs):
+        __import__('sys').stdout.write(*args, *kwargs)
+
+
+Factorial.siso(Factorial().call_both(3)) # call class instance and print the value
